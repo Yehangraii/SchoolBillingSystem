e081ebc732e5cd2ff9cfe960ace9cc378bf95669
kriipa <kripathapa2002>
2021-09-17 23:21:59 +0545

    final commit

--- src/project/Login.java
+++ src/project/Login.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Login implements ActionListener {
     JFrame frame;
@@ -15,86 +17,104 @@ public class Login implements ActionListener {
     JPanel pan;
     Font fn, fn1;
 
-        public Login() {
-            frame = new JFrame("LMS");
+    public Login() {
+        frame = new JFrame("LMS");
 
-            fn = new Font("Cambria", Font.BOLD, 25);
-            fn1 = new Font("Cambria", Font.ITALIC, 15);
+        fn = new Font("Cambria", Font.BOLD, 25);
+        fn1 = new Font("Cambria", Font.ITALIC, 15);
 
-            txt_title = new JLabel("Login Credentials");
-            txt_title.setFont(fn);
-            txt_title.setForeground(Color.decode("#A7C0C8"));
-            txt_title.setBounds(250, 50, 250, 30);
+        txt_title = new JLabel("Login Credentials");
+        txt_title.setFont(fn);
+        txt_title.setForeground(Color.decode("#A7C0C8"));
+        txt_title.setBounds(250, 50, 250, 30);
 
-            lbl_username = new JLabel("User Name");
-            lbl_username.setFont(fn1);
-            lbl_username.setBounds(310, 150, 100, 20);
+        lbl_username = new JLabel("Email");
+        lbl_username.setFont(fn1);
+        lbl_username.setBounds(310, 150, 100, 20);
 
-            lbl_password = new JLabel("Password");
-            lbl_password.setFont(fn1);
-            lbl_password.setBounds(310, 260, 100, 20);
+        lbl_password = new JLabel("Password");
+        lbl_password.setFont(fn1);
+        lbl_password.setBounds(310, 260, 100, 20);
 
-            txt_username = new JTextField();
-            txt_username.setBounds(240, 190, 210, 40);
+        txt_username = new JTextField();
+        txt_username.setBounds(240, 190, 210, 40);
 
-            txt_password = new JPasswordField();
-            txt_password.setBounds(240, 300, 210, 40);
+        txt_password = new JPasswordField();
+        txt_password.setBounds(240, 300, 210, 40);
 
-            btn_login = new JButton("Login");
-            btn_login.setForeground(Color.darkGray);
-            btn_login.setBackground(Color.CYAN);
-            btn_login.setBounds(240, 370, 100, 50);
-            btn_login.addActionListener(this);
-            frame.add(btn_login);
+        btn_login = new JButton("Login");
+        btn_login.setForeground(Color.darkGray);
+        btn_login.setBackground(Color.CYAN);
+        btn_login.setBounds(240, 370, 100, 50);
+        btn_login.addActionListener(this);
+        frame.add(btn_login);
 
-            btn_register = new JButton("Register");
-            btn_register.setForeground(Color.darkGray);
-            btn_register.setBackground(Color.CYAN);
-            btn_register.setBounds(360, 370, 100, 50);
-            btn_register.addActionListener(this);
+        btn_register = new JButton("Register");
+        btn_register.setForeground(Color.darkGray);
+        btn_register.setBackground(Color.CYAN);
+        btn_register.setBounds(360, 370, 100, 50);
+        btn_register.addActionListener(this);
 
-            frame.add(btn_register);
-            frame.add(txt_title);
-            frame.add(lbl_username);
-            frame.add(lbl_password);
-            frame.add(txt_password);
-            frame.add(txt_username);
+        frame.add(btn_register);
+        frame.add(txt_title);
+        frame.add(lbl_username);
+        frame.add(lbl_password);
+        frame.add(txt_password);
+        frame.add(txt_username);
 
-            // JPanel
-            pan = new JPanel();
-            pan = new JPanel();
-            pan.setLayout(null);
+        // JPanel
+        pan = new JPanel();
+        pan = new JPanel();
+        pan.setLayout(null);
 //            pan.setSize(450, 300);
-            pan.setVisible(true);
-            pan.setBackground(Color.decode("#A7C0C8"));
-            pan.setBounds(125, 100, 450, 350);
-            pan.setBorder(BorderFactory.createMatteBorder
-                    (8, 8, 8, 8, Color.decode("#090C09")));
+        pan.setVisible(true);
+        pan.setBackground(Color.decode("#A7C0C8"));
+        pan.setBounds(125, 100, 450, 350);
+        pan.setBorder(BorderFactory.createMatteBorder
+                (8, 8, 8, 8, Color.decode("#090C09")));
 
-            frame.add(pan);
+        frame.add(pan);
 
-            frame.getContentPane().setBackground( Color.decode("#276891"));
-            frame.setLayout(null);
-            frame.setSize(700, 550);
-            frame.setVisible(true);
-            frame.setLocation(380,200);
-        }
-        public static void main(String[] Args) {
-            new Login();
-        }
+        frame.getContentPane().setBackground(Color.decode("#276891"));
+        frame.setLayout(null);
+        frame.setSize(700, 550);
+        frame.setVisible(true);
+        frame.setLocation(380, 200);
+    }
+
+    public static void main(String[] Args) {
+        new Login();
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource()==btn_register ){
-            new RegisterPage();
-            frame.dispose();
-        }
-        else if (e.getSource() == btn_login){
 
-            String username = txt_username.getText();
-            String password = txt_password.getText();
-//            String query = " Select "
+        String username = txt_username.getText();
+        String password = txt_password.getText();
+
+        if (e.getSource()==btn_login){
+            String query = "Select email,password from register where email='"+username+"'and password ='"
+                    +password+"'";
+            DataBase db =new DataBase();
+            ResultSet rs= db.select(query);
+            try {
+                if (username.length()==0 && password.length()==0){
+                    JOptionPane.showMessageDialog(btn_login,"Please! Re-enter your information.");
+                }else if (rs.next()){
+                    JOptionPane.showMessageDialog(btn_login,"Login Successfully.");
+                    new Dashboard();
+                    frame.dispose();
+                }else{
+                    JOptionPane.showMessageDialog(btn_login,"Please! Enter Correct Information.");
+                }
+            }catch (SQLException throwables){
+                throwables.printStackTrace();
+            }
         }
+          else if (e.getSource() == btn_register) {
+              new RegisterPage();
+              frame.dispose();
+          }
     }
 
 
